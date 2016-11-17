b1bc1e500a0064d84b627c4920283135f5d36770
Caleb Paul and Zachary Matthewstearn <caleb@>
2016-11-17 10:13:42 -0800

    add storeActivity

--- app/src/main/java/com/app/fantasycostco/StoreActivity.java
+++ app/src/main/java/com/app/fantasycostco/StoreActivity.java
@@ -2,16 +2,12 @@ package com.app.fantasycostco;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
 
-import butterknife.Bind;
-
-public class MainActivity extends AppCompatActivity {
-    @Bind(R.id.button_start) Button mButtonStart;
+public class StoreActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_store);
     }
 }
