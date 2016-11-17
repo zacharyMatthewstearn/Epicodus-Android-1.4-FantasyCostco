4e2b0d93b900e1f801e687a95fa1e2ee0ceac165
Caleb Paul and Zachary Matthewstearn <caleb@>
2016-11-17 15:26:59 -0800

    pass item name into dialog fragment

--- app/src/main/java/com/app/fantasycostco/StoreActivity.java
+++ app/src/main/java/com/app/fantasycostco/StoreActivity.java
@@ -1,6 +1,8 @@
 package com.app.fantasycostco;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,7 +16,7 @@ import android.widget.Toast;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class StoreActivity extends AppCompatActivity {
+public class StoreActivity extends AppCompatActivity implements ItemDetailFragment.OnFragmentInteractionListener {
     @Bind(R.id.itemsListView) ListView mItemsListView;
 
     String[] inventory;
@@ -33,8 +35,18 @@ public class StoreActivity extends AppCompatActivity {
         mItemsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(StoreActivity.this, "Clicked!", Toast.LENGTH_SHORT).show();
+                DialogFragment newDetail = new ItemDetailFragment();
+                Bundle args = new Bundle();
+                args.putString("itemName", parent.getItemAtPosition(position).toString());
+                newDetail.setArguments(args);
+                newDetail.show(getSupportFragmentManager(), "detail");
+                //Toast.makeText(StoreActivity.this, "Clicked: " + parent.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
