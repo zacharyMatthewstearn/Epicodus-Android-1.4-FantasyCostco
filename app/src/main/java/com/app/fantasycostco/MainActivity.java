1b4b2e1c91e512b4e86c3daf025b0fb99483ffdf
Caleb Paul and Zachary Matthewstearn <caleb@>
2016-11-17 11:22:28 -0800

    bind array to store activity

--- app/src/main/java/com/app/fantasycostco/MainActivity.java
+++ app/src/main/java/com/app/fantasycostco/MainActivity.java
@@ -1,10 +1,13 @@
 package com.app.fantasycostco;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
     @Bind(R.id.button_start) Button mButtonStart;
@@ -13,5 +16,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mButtonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, StoreActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
+
+
 }
--- app/src/main/java/com/app/fantasycostco/StoreActivity.java
+++ app/src/main/java/com/app/fantasycostco/StoreActivity.java
@@ -2,12 +2,25 @@ package com.app.fantasycostco;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class StoreActivity extends AppCompatActivity {
+    @Bind(R.id.itemsListView) ListView mItemsListView;
+    private String[] inventory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store);
+        ButterKnife.bind(this);
+
+        inventory = getResources().getStringArray(R.array.item_names);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, inventory);
+        mItemsListView.setAdapter(adapter);
     }
 }
